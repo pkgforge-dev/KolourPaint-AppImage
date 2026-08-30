96d1737204f3090acb78b00582d816217306a381
Link4Electronics <link4electronics@gmail.com>
2026-08-30 04:01:22 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/kolourpaint.png
 export DESKTOP=/usr/share/applications/org.kde.kolourpaint.desktop
-export DEPLOY_QT=1
-export QT_DIR=qt6
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/kolourpaint /usr/lib/libkolourpaint*.so* /usr/share/kolourpaint /usr/lib/qt6/plugins/kf6
