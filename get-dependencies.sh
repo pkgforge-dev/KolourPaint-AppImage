bc6debebc2870545cf600b2e2edc6da6b411a578
Link4Electronics <link4electronics@gmail.com>
2026-09-12 00:22:46 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    kolourpaint   \
-    kvantum       \
-    lxqt-qtplugin \
-    qt6ct
+pacman -Syu --noconfirm kolourpaint kvantum lxqt-qtplugin qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
